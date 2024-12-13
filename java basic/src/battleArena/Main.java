f262c3f0f459630dcca144c646f6041e3bcb025b
ITakora <oyamitakora@gmail.com>
2024-12-13 21:52:02 +0700

    belajar java oop

---
+++ java basic/src/battleArena/Main.java
@@ -0,0 +1,17 @@
+package battleArena;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Enemy enemy = new Enemy();
+
+        enemy.typeOfEnemy = "Zombie";
+
+        System.out.println(enemy.typeOfEnemy + " has " + enemy.healthPoints + " health points and can do attack of " + enemy.attackDamage);
+
+        enemy.talk();
+        enemy.walkForward();
+        enemy.attack();
+
+    }
+}
